3aeba4d4878bcc2b61447246bbb5b44b15df62a0
Chandrakant <chandrakant@ChandrakantsMBP.lan>
2024-01-23 16:40:29 -0500

    02. code refactor

--- src/main/java/com/learnwithchandrakant/ratingsdataservice/resource/RatingResource.java
+++ src/main/java/com/learnwithchandrakant/ratingsdataservice/resource/RatingResource.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratingdata")
 public class RatingResource {
@@ -13,5 +16,19 @@ public class RatingResource {
     @GetMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
         return new Rating("TNS","5");
+    }
+
+    @GetMapping("/users/{userId}")
+    public UserRating getUsersRating(@PathVariable("userId") String userId){
+      List<Rating>  ratings =Arrays.asList(
+                new Rating("1234","3"),
+                new Rating("5678","4"),
+                new Rating("8910","5")
+        );
+
+      UserRating userRating= new UserRating();
+      userRating.setUserRating(ratings);
+
+      return userRating;
     }
 }
